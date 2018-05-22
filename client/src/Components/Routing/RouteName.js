f6746310bc9d95c130f83515bda6b884816ee408
Phạm Ngọc Duy <phamngocduy98@gmail.com>
2018-05-22 20:58:47 +0700

    Page Title responsive

--- client/src/Components/Routing/RouteName.js
+++ client/src/Components/Routing/RouteName.js
@@ -10,16 +10,16 @@ class RouteName extends Component {
         return (
             <div>
                 <Switch>
-                    <Route exact path='/' render={() => <div>Trang chủ</div>} />
-                    <Route path='/timkiem' render={() => <div>Tìm kiếm</div>} />
-                    <Route path='/baidang' render={() => <div>Bài đăng</div>} />
-                    <Route path='/theodoi' render={() => <div>Theo dõi</div>} />
-                    <Route path='/giangvien' render={() => <div>Giảng viên</div>} />
-                    <Route path='/partner' render={() => <div>Partner</div>} />
-                    <Route path='/hocvien' render={() => <div>Học viên</div>} />
-                    <Route path='/baocao' render={() => <div>Báo cáo</div>} />
-                    <Route path='/tinnhan' render={() => <div>Tin nhắn</div>} />
-                    <Route path='/profile' render={() => <div>Hồ sơ</div>} />
+                    <Route exact path='/' render={() => { document.title = "Trang chủ - Quản lí thực tập"; return <div>Trang chủ</div>} } />
+                    <Route path='/timkiem' render={() => { document.title = "Tìm kiếm - Quản lí thực tập"; return <div>Tìm kiếm</div>} } />
+                    <Route path='/baidang' render={() => { document.title = "Bài Đăng - Quản lí thực tập"; return <div>Bài Đăng</div>} } />
+                    <Route path='/theodoi' render={() => {document.title = "Đang theo dõi - Quản lí thực tập"; return <div>Theo dõi</div>} } />
+                    <Route path='/giangvien' render={() => { document.title = "Giảng viên - Quản lí thực tập"; return <div>Giảng viên</div>} } />
+                    <Route path='/partner' render={() => { document.title = "Partner - Quản lí thực tập"; return <div>Partner</div>} } />
+                    <Route path='/hocvien' render={() => { document.title = "Học viên - Quản lí thực tập"; return <div>Học viên</div>} } />
+                    <Route path='/baocao' render={() => { document.title = "Báo cáo - Quản lí thực tập"; return <div>Báo cáo</div>} } />
+                    <Route path='/tinnhan' render={() => { document.title = "Tin nhắn - Quản lí thực tập"; return <div>Tin nhắn</div>} } />
+                    <Route path='/profile' render={() => { document.title = "Hồ sơ - Quản lí thực tập"; return <div>Hồ sơ</div>} } />
                 </Switch>
             </div>
         );
