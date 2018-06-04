083d7566d0c08a46cc080c610a20c3468d043048
vukihai <vukihai98@gmail.com>
2018-06-04 20:11:03 +0700

    -tin nhan

--- api/Messages/MessagesModel.php
+++ api/Messages/MessagesModel.php
@@ -8,31 +8,27 @@
 */
     class MessagesModel extends Model {
         public function getListMessages($fieldsArr, $id){
-            $data = $this->db->doPreparedQuery("SELECT message.id, vnuaccount.fullName as sender, message.subject, message.sendTime, message.seen FROM message join vnuaccount on message.senderID=vnuaccount.VNUid WHERE message.receiverID=? ORDER BY message.sendTime LIMIT 20",array($id));
+            $data = $this->db->doPreparedQuery("SELECT message.id, vnuaccount.fullName as sender, message.subject, message.sendTime, message.seen FROM message join vnuaccount on message.senderID=vnuaccount.VNUid WHERE message.receiverID=? ORDER BY message.sendTime desc LIMIT 20",array($id));
             
             return $this->fieldsFilterForArray($fieldsArr, $data);
         }
         public function getMessage($fieldsArr, $stu_id, $mess_id){
             $data = array();
-            // tin nhan hien tai
-            $thisMessage = $this->db->doPreparedQuery("SELECT message.id, message.parentID, vnuaccount.fullName as sender, message.subject,  message.content, message.attachment, message.sendTime, message.seen FROM message join vnuaccount on message.senderID=vnuaccount.VNUid WHERE message.receiverID=? AND message.id=?",array($stu_id,$mess_id));
-            
+            //tim id thu root
+            $thisMessage = $this->db->doPreparedQuery("SELECT message.id, message.parentID FROM message WHERE message.receiverID=? AND message.id=?",array($stu_id,$mess_id));
             if($thisMessage[0]["parentID"] != null) {
-                $re = $this->db->doPreparedQuery("SELECT message.id, vnuaccount.fullName as sender, message.subject,  message.content, message.attachment, message.sendTime, message.seen FROM message join vnuaccount on message.senderID=vnuaccount.VNUid WHERE message.receiverID=? AND message.parentID=? ORDER BY message.sendTime DESC limit 20",array($stu_id,$thisMessage[0]["parentID"]));
-                foreach($re as $i) {
-                array_push($data, $i);
-                }
-                //tin root
-                $root = $this->db->doPreparedQuery("SELECT message.id, message.parentID, vnuaccount.fullName as sender, message.subject,  message.content, message.attachment, message.sendTime, message.seen FROM message join vnuaccount on message.senderID=vnuaccount.VNUid WHERE message.receiverID=? AND message.id=?",array($stu_id,$thisMessage[0]["parentID"]));
-                array_push($data, $root[0]);
+                $root = $thisMessage[0]["parentID"];
             } else {
-                $re = $this->db->doPreparedQuery("SELECT message.id, vnuaccount.fullName as sender, message.subject,  message.content, message.attachment, message.sendTime, message.seen FROM message join vnuaccount on message.senderID=vnuaccount.VNUid WHERE message.receiverID=? AND message.parentID=? ORDER BY message.sendTime DESC limit 20",array($stu_id,$thisMessage[0]["id"]));
-                foreach($re as $i) {
-                array_push($data, $i);
-                }
-                array_push($data, $thisMessage[0]);
+                $root = $thisMessage[0]["id"];
             }
-            
+            // lay cac thu cung root
+            $re = $this->db->doPreparedQuery("SELECT message.id, vnuaccount.fullName as sender, message.subject,  message.content, message.attachment, message.sendTime, message.seen FROM message join vnuaccount on message.senderID=vnuaccount.VNUid WHERE message.parentID=? ORDER BY message.sendTime DESC limit 20",array($root));
+            foreach($re as $i) {
+                array_push($data, $i);
+            }
+            // lay root
+            $rootMess = $this->db->doPreparedQuery("SELECT message.id, vnuaccount.fullName as sender, message.subject,  message.content, message.attachment, message.sendTime, message.seen FROM message join vnuaccount on message.senderID=vnuaccount.VNUid WHERE message.id=?",array($root));
+            array_push($data, $rootMess[0]);
             return $data;
         }
     }
